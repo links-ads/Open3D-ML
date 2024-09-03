de170f536159152b2eac3bde4cfbb10d6b8409c0
gaetanochiriaco <gaetano.chiriaco@linksfoundation.com>
2024-09-03 11:29:04 +0200

    major fix to sampler and added class sampler

--- ml3d/datasets/samplers/__init__.py
+++ ml3d/datasets/samplers/__init__.py
@@ -1,6 +1,10 @@
 """Various algorithms for sampling points from input point clouds."""
 
-from .semseg_random import SemSegRandomSampler
+from .semseg_random import SemSegRandomSampler, SemSegRandomClassSampler
 from .semseg_spatially_regular import SemSegSpatiallyRegularSampler
 
-__all__ = ['SemSegRandomSampler', 'SemSegSpatiallyRegularSampler']
+__all__ = [
+    "SemSegRandomSampler",
+    "SemSegSpatiallyRegularSampler",
+    "SemSegRandomClassSampler",
+]
--- ml3d/datasets/samplers/semseg_random.py
+++ ml3d/datasets/samplers/semseg_random.py
@@ -2,6 +2,9 @@ import numpy as np
 import random
 
 from ...utils import SAMPLER
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class SemSegRandomSampler(object):
@@ -31,17 +34,19 @@ class SemSegRandomSampler(object):
     def get_point_sampler():
 
         def _random_centered_gen(**kwargs):
-            pc = kwargs.get('pc', None)
-            num_points = kwargs.get('num_points', None)
-            search_tree = kwargs.get('search_tree', None)
+            pc = kwargs.get("pc", None)
+            num_points = kwargs.get("num_points", None)
+            search_tree = kwargs.get("search_tree", None)
             if pc is None or num_points is None or search_tree is None:
-                raise KeyError("Please provide pc, num_points, and search_tree \
-                    for point_sampler in SemSegRandomSampler")
+                raise KeyError(
+                    "Please provide pc, num_points, and search_tree \
+                    for point_sampler in SemSegRandomSampler"
+                )
 
             center_idx = np.random.choice(len(pc), 1)
             center_point = pc[center_idx, :].reshape(1, -1)
 
-            if (pc.shape[0] < num_points):
+            if pc.shape[0] < num_points:
                 diff = num_points - pc.shape[0]
                 idxs = np.array(range(pc.shape[0]))
                 idxs = list(idxs) + list(random.choices(idxs, k=diff))
@@ -49,10 +54,84 @@ class SemSegRandomSampler(object):
             else:
                 idxs = search_tree.query(center_point, k=num_points)[1][0]
             random.shuffle(idxs)
-            pc = pc[idxs]
-            return pc, idxs, center_point
+            # pc = pc[idxs]
+            return idxs, center_point
 
         return _random_centered_gen
 
 
 SAMPLER._register_module(SemSegRandomSampler)
+
+
+class SemSegRandomClassSampler(object):
+    """Random sampler for semantic segmentation datasets."""
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.length = len(dataset)
+        self.split = self.dataset.split
+
+    def __len__(self):
+        return self.length
+
+    def initialize_with_dataloader(self, dataloader):
+        self.length = len(dataloader)
+
+    def get_cloud_sampler(self):
+
+        def gen():
+            ids = np.random.permutation(self.length)
+            for i in ids:
+                yield i
+
+        return gen()
+
+    @staticmethod
+    def get_point_sampler():
+
+        def _random_centered_gen(**kwargs):
+            pc = kwargs.get("pc", None)
+            label = kwargs.get("label", None)
+            num_points = kwargs.get("num_points", None)
+            search_tree = kwargs.get("search_tree", None)
+            sampler = kwargs.get("sampler", None)
+            if (
+                pc is None
+                or num_points is None
+                or search_tree is None
+                or sampler is None
+            ):
+                raise KeyError(
+                    "Please provide pc, num_points, search_tree and sampler \
+                    for point_sampler in SemSegRandomClassSampler"
+                )
+            # unique_labels = np.unique(label)
+            class_list = list(sampler["classes"])
+            while True:
+                random_class = np.random.choice(range(len(class_list)), 1)
+                log.info(f"Sampled class: {random_class}")
+                idx_class = np.where(label == random_class)[0]
+                if len(idx_class) == 0:
+                    log.info(f"No points for class {random_class}")
+                    class_list.remove(random_class)
+                    continue
+                break
+            center_idx = np.random.choice(idx_class, 1)
+            # center_idx = np.random.choice(len(pc), 1)
+            center_point = pc[center_idx, :].reshape(1, -1)
+
+            if pc.shape[0] < num_points:
+                diff = num_points - pc.shape[0]
+                idxs = np.array(range(pc.shape[0]))
+                idxs = list(idxs) + list(random.choices(idxs, k=diff))
+                idxs = np.asarray(idxs)
+            else:
+                idxs = search_tree.query(center_point, k=num_points)[1][0]
+            random.shuffle(idxs)
+
+            return idxs, center_point
+
+        return _random_centered_gen
+
+
+SAMPLER._register_module(SemSegRandomClassSampler)
